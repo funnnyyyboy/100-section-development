02669be752b1c72fe76dedafdc2a6bcd58f7b939
funnnyyyboy <funnnyyyboy@gmail.com>
2017-05-31 08:59:28 +0600

    Add files via upload

---
+++ Assignment class 3 full width video/assets/js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        
+        $('.video-play-btn').magnificPopup({
+            'type' : 'video',
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+//
+//        $(".embed-responsive iframe").addClass("embed-responsive-item");
+//        $(".carousel-inner .item:first-child").addClass("active");
+//        
+//        $('[data-toggle="tooltip"]').tooltip();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
